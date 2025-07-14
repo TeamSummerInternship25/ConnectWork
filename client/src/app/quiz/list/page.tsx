c45329c33b0dd0b60207d7b175d79cc0a640001a
Ashley191-cyber <qiushigong191@gmail.com>
2025-07-14 09:45:00 +0800

    feat: Create quiz list page structure

---
+++ client/src/app/quiz/list/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'; 
+ 
+export default function QuizListPage() { 
+  return ( 
+    <div className="container mx-auto px-4 py-8"> 
+      <h1 className="text-2xl font-bold mb-6">Quiz Management</h1> 
+      <div className="bg-white rounded-lg shadow p-6"> 
+        <p>Quiz list will be displayed here</p> 
+      </div> 
+    </div> 
+  ); 
+} 
